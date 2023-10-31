9a50d5707c86f4bb9c17f324fac07f1ae0f07e07
devmarcosav <devmarcosav@gmail.com>
2023-10-31 18:44:49 -0300

    feat: add main api

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/marcosavieira/go-finance/api"
+	db "github.com/marcosavieira/go-finance/db/sqlc"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSourced     = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
+	serverAddress = "0.0.0.0:8000"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSourced)
+	if err != nil {
+		log.Fatal("cannot connect to database", err)
+		return
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start api", err)
+		return
+	}
+}
