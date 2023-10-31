fa0e22f7c24b0959e8c3d37c76b99202e1c3ae05
devmarcosav <devmarcosav@gmail.com>
2023-10-31 18:43:01 -0300

    feat: add server api

---
+++ api/server.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/marcosavieira/go-finance/db/sqlc"
+)
+
+type Server struct {
+	store  *db.SQLStore
+	router *gin.Engine
+}
+
+func NewServer(store *db.SQLStore) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/user", server.createUser)
+	router.GET("/user/:username", server.getUser)
+	router.GET("/user/:id", server.getUserById)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"api has error response:": err.Error()}
+}
