295c44d04a64baf9ab36d98277750c381dbe6070
devmarcosav <devmarcosav@gmail.com>
2023-11-01 15:35:15 -0300

    refactor: variaveis de ambiente

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,17 +6,21 @@ import (
 	"os"
 	"testing"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver  = "postgres"
-	dbSourced = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
 )
 
 var testQueries *Queries
 
 func TestMain(m *testing.M) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading godotenv: ", err)
+	}
+
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSourced := os.Getenv("DB_SOURCED")
+
 	conn, err := sql.Open(dbDriver, dbSourced)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
