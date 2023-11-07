47b1ebe049592dc0b26e04e1290e0ddecdb6ba08
unknown <devmarcosav@gmail.com>
2023-11-07 19:54:02 -0300

    feat: create auth and token

---
+++ api/auth.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/marcosavieira/go-finance/api/security"
+)
+
+type loginRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func (server *Server) userLogin(ctx *gin.Context) {
+	var req loginRequest
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	err = security.VerificarSenha(user.Password, req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	
+
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["exp"] = time.Now().Add(time.Minute * 100).Unix()
+	claims["username"] = req.Username
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte("secret_key"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, tokenString)
+}
